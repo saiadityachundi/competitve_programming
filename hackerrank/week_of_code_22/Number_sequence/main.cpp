6b8a27e10fb0beaf8cde594f77d7a66c0958403e
Sai Aditya Chundi <saiaditya.chundi@gmail.com>
2016-08-20 23:38:12 +0530

    Next version, albeit a broken one!!!

--- hackerrank/week_of_code_22/Number_sequence/main.cpp
+++ hackerrank/week_of_code_22/Number_sequence/main.cpp
@@ -9,15 +9,15 @@
 
 using namespace std;
 
-int next(map<long, vector<long> > & newmap, map<long, vector<long>::iterator > & mit, long kind, long ar[]){
+int next(map<long, vector<long> > & newmap, map<long, vector<long>::iterator > & mit, long kind){
 
-    map<long, vector <long>>::reverse_iterator riter(newmap.find(kind));
-    /*
+    if(newmap.count(kind)==0)
+        return 0;
     map<long, vector <long>>::reverse_iterator riter(newmap.find(kind));
     map<long, vector <long>>::iterator it1;
     long ind = riter->first;
     mit[ind]++;
-
+    
     while(mit[ind]==newmap[ind].end() && riter!=newmap.rend()){
         ind=riter->first;
         mit[ind]=newmap[ind].begin();
@@ -25,9 +25,9 @@ int next(map<long, vector<long> > & newmap, map<long, vector<long>::iterator > &
         ind=riter->first;
         mit[ind]++;
     }
-
+    
     if(riter==newmap.rend())
-        return -1;
+        return 0;
 
     map<long, vector<long>>::iterator iter=newmap.find(riter->first);
     map<long, vector<long>>::iterator iter0=newmap.begin();
@@ -35,45 +35,25 @@ int next(map<long, vector<long> > & newmap, map<long, vector<long>::iterator > &
     long r;
     long j,jr;
     vector <long>::iterator it2;
-
-    for(iter0 = newmap.begin();iter0!=newmap.end();iter0++){
-        i = iter0->first;
-        r = *mit[i];
-        it1=iter;
-        for(it1=it1;it1!=newmap.end();it1++){
-            j=it1->first;
+    
+    for(iter0 = iter;iter0!=newmap.end();iter0++){
+        for(it1=newmap.begin();it1!=iter0;it1++){
+            i = it1->first;
+            r = *mit[i];
+            j=iter0->first;
             if(j%i==0){
-                it2 = newmap[j].begin();
+                it2 = mit[j];
                 while(*it2%i != r && it2 != newmap[j].end())
                     it2++;
                 if(it2==newmap[j].end()){
-                    return next(newmap, mit, i);
+                    mit[j]=newmap[j].begin();
+                    return next(newmap, mit, j-1);
                 }
+                mit[j]=it2;
             }
         }
     }
-
-    //======================================================
-    for(iter=iter;iter!=newmap.end();iter++){
-        i = iter->first;
-        r = *mit[i];
-        it1=iter;
-        it1++;
-        for(it1=it1;it1!=newmap.end();it1++){
-            j=it1->first;
-            if(j%i==0){
-                it2 = newmap[j].begin();
-                while(*it2%i != r && it2 != newmap[j].end())
-                    it2++;
-                if(it2==newmap[j].end()){
-                    return next(newmap, mit, i);
-                }
-            }
-        }
-    }
-    //=====================================================
-    */
-    return 0;
+    return kind;
 }
 
 int main() {
@@ -87,13 +67,13 @@ int main() {
             m[i+1]=vector<long>();
         }
     }
-
+    
     vector<long>::iterator it;
     long z;
     bool made_empty[m.size()];
-
+    
     long j,p;
-
+    
     for(long i=1;i<=n;i++){
         if(a[i-1]!=-1){
             j=1;
@@ -127,7 +107,7 @@ int main() {
             }
         }
     }
-
+    
     for(map<long, vector <long> >::iterator iter=m.begin();iter!=m.end();iter++){
         long n = iter->first;
         if(m[n].empty()){
@@ -137,28 +117,40 @@ int main() {
         }
     }
 
+    map<long, vector<long>> newmap=m;
+    map<long, vector<long>::iterator> mit;
+    
+    for(map<long, vector <long>>::iterator iter=newmap.begin();iter != newmap.end();iter++){
+        mit[iter->first]=newmap[iter->first].begin();
+    }
+    
+    long i,r;
+
     long c=0;
-    p=1;
-
-    map<long, vector <long>>::iterator itr;
-    for(itr=m.begin();itr!=m.end();itr++){
-        long f=itr->first;
-        c=0;
-        for(it=m[f].begin();it!=m[f].end();it++){
-        c++;
-        c%=mod;
+    bool isBreak=false;
+    for(map<long, vector<long>>::iterator iter=newmap.begin();iter!=newmap.end();iter++){
+        for(map<long, vector<long>>::iterator iter1=newmap.begin();iter1!=iter;iter1++){
+            j=iter->first;
+            i=iter1->first;
+            r=*mit[i];
+            if(j%i==0 && *mit[j]%i!=r){
+                isBreak = true;
+                break;
+            }
         }
-        p*=c;
-        p%=mod;
+        if(isBreak)
+            break;
     }
-    cout << p;
-
-    /*map<long, vector <long>> newmap;
-    for(map<long, vector<long> >::iterator it=m.begin();it!=m.end();it++){
-        long in = it->first;
-        newmap[in]=m[in];
-    }
-
-    long sz = m.size();
-    vector <long>::iterator at[sz+1];*/
+    if(!isBreak)
+        c=1;
+    
+    long index;
+    map<long, vector<long>>::reverse_iterator riter = newmap.rbegin();
+    index = riter->first;
+    
+    while(index=next(newmap, mit, index))
+        c++;
+    
+    cout << c;
+    
 }
