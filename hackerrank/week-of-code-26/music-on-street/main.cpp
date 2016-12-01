003df927991488dcdf075996952e764b0c581401
Sai Aditya Chundi <saiaditya.chundi@gmail.com>
2016-12-02 04:22:38 +0530

    Added a cpp version of the code. But it's not working. Seems like there's a bug in it.

---
+++ hackerrank/week-of-code-26/music-on-street/main.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int n,m,hmx,hmn,*a;
+int **op;
+
+int opf(int i,int ms){
+    if(op[i][ms]!=-1)
+        return op[i][ms];
+    else if(i==n-1)
+        if(ms>=hmn && ms<=hmx){
+            op[i][ms]=1;
+            return op[i][ms];
+        }
+        else{
+            op[i][ms]=0;
+            return op[i][ms];
+        }
+    else if(ms<hmn){
+        op[i][ms]=0;
+        return op[i][ms];
+    }
+    else if(a[i+1]-a[i]>=hmn && a[i+1]-a[i]<=hmx){
+        op[i][ms]=opf(i+1, ms-(a[i+1]-a[i]));
+        return op[i][ms];
+    }
+    else{
+        op[i][ms]=0;
+        return op[i][ms];
+    }
+}
+
+int func(int i,int ms){
+    int z,j;
+    if(i==0)
+        z=hmx;
+    else
+        z=min(hmx,a[i]-a[i-1]);
+    j=hmn;
+    while(j<=z){
+        if(opf(i,ms-j))
+            return a[i]-j;
+        j+=1;
+    }
+    return func(i+1,ms);
+}
+
+int main(){
+
+    cin >> n;
+
+    a=(int *)malloc(n*sizeof(int));
+
+    for(int i=0;i<n;i++)
+        cin >> a[i];
+    
+    cin >> m >> hmx >> hmn;
+
+    op=new int*[n];
+    for(int i=0;i<n;i++){
+        op[i]=new int[m+1];
+        for(int j=0;j<m+1;j++)
+            op[i][j]=-1;
+    }
+
+    cout << func(0,m);
+
+    return 0;
+}
