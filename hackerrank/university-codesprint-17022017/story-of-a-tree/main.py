c011b721c1e22f9ee0be402543c58452c667d21c
Sai Aditya Chundi <saiaditya.chundi@gmail.com>
2017-02-21 22:21:36 +0530

    Final working. fixed a bug. All test cases pass

--- hackerrank/university-codesprint-17022017/story-of-a-tree/main.py
+++ hackerrank/university-codesprint-17022017/story-of-a-tree/main.py
@@ -1,88 +1,76 @@
-#!/bin/python2
+#!/usr/bin/python2
 
 from fractions import gcd
 
-import sys
+t=int(raw_input())
+for t0 in xrange(t):
+    tr={}
+    p={}
+    c={}
+    n=int(raw_input())
+    for t1 in xrange(n-1):
+        s=map(int, raw_input().split())
+        if not(tr.has_key(s[0])):
+            tr[s[0]]=[]
+        if not(tr.has_key(s[1])):
+            tr[s[1]]=[]
+        tr[s[0]].append(s[1])
+        tr[s[1]].append(s[0])
+    q=[1]
+    vst={}
+    while(len(q)!=0):
+        nd=q.pop(0)
+        vst[nd]=1
+        for nb in tr[nd]:
+            if not(vst.has_key(nb)):
+                q.append(nb)
+                p[nb]=nd
 
-tr={}
-gu=[]
-p={}
-c={}
-cnd={}
+    s=map(int, raw_input().split())
+    g=s[0]
+    k=s[1]
+    gu={}
+    for t2 in xrange(g):
+        s=map(int, raw_input().split())
+        if(gu.has_key((s[1],s[0]))):
+            k-=1
+            del gu[(s[1],s[0])]
+        else:
+            gu[(s[0],s[1])]=1
 
-def cndd(nd):
-    if(cnd.has_key(nd)):
-        return cnd[nd]
-    if(gu.has_key((p[nd],nd))):
-        cnd[nd]=cndd(p[nd])-1
-    elif(gu.has_key((nd,p[n]))):
-        cnd[nd]=cndd(p[nd])+1
-    else:
-        cnd[nd]=cndd(p[nd])
-    return cnd[nd]
+    cnd={}
 
-q = int(raw_input().strip())
-for a0 in xrange(q):
-        n = int(raw_input().strip())
-        tr={}
-        p={}
-        c={}
-        cnd={}
-        for a1 in xrange(n-1):
-                u,v = raw_input().strip().split(' ')
-                u,v = [int(u),int(v)]
-                if not (tr.has_key(u)):
-                        tr[u]=[]
-                tr[u].append(v)
-                if not (tr.has_key(v)):
-                        tr[v]=[]
-                tr[v].append(u)
-        q=[1]
-        vst={}
-        vst[1]=1
-        while len(q)!=0:
-            nd=q.pop(0)
-            vst[nd]=1
-            if(not c.has_key(nd)):
-                c[nd]=[]
-            for i in tr[nd]:
-                if(not vst.has_key(i)):
-                    c[nd].append(i)
-                    p[i]=nd
-                    q.append(i)
+    cnd[1]=0
 
-        g,k = raw_input().strip().split(' ')
-        g,k = [int(g),int(k)]
-        gu={}
-        for a1 in xrange(g):
-                u,v=raw_input().split(' ')
-                u,v=[int(u),int(v)]
-                gu[(u,v)]=1
-
-        cnt=0
-
-        cnd[1]=0
-        for i in gu.keys():
-            x,y=i
-            if(y!=1 and x==p[y]):
+    for ge in gu.keys():
+        x,y=ge
+        if(y!=1):
+            if(p[y]==x):
                 cnd[1]+=1
 
-        if(cnd[1]>=k):
+    q=[1]
+    vst={}
+    while(len(q)!=0):
+        nd=q.pop(0)
+        vst[nd]=1
+        if(nd!=1):
+            if(gu.has_key((p[nd],nd))):
+                cnd[nd]=cnd[p[nd]]-1
+            elif(gu.has_key((nd,p[nd]))):
+                cnd[nd]=cnd[p[nd]]+1
+            else:
+                cnd[nd]=cnd[p[nd]]
+ 
+        for nb in tr[nd]:
+            if not(vst.has_key(nb)):
+                q.append(nb)
+
+    cnt=0
+    for i in xrange(n):
+        if(cnd[i+1]>=k):
             cnt+=1
 
-        i=2
-        while(i<=n):
-            if(cndd(i)>=k):
-                cnt+=1
-            i+=1
-
-        gc=gcd(cnt,n)
-        cnt/=gc
-        n/=gc
-        print str(cnt)+"/"+str(n)
-
-#        print "xxxxxx"
-#        print p
-#        print c
-#        print cnd
-#        print "xxxxxx"
+    gc=gcd(cnt,n)
+    cnt/=gc
+    n/=gc
+    print str(cnt)+"/"+str(n)
