c1671e2e20dd041945c86a05d99fad6e7acf856f
Sai Aditya Chundi <saiaditya.chundi@gmail.com>
2017-04-15 12:35:52 +0530

    Another version of the spanningTree Problem.

--- hackerrank/week-of-code-31/spanningTreeFraction/main.py
+++ hackerrank/week-of-code-31/spanningTreeFraction/main.py
@@ -14,40 +14,73 @@ for i in xrange(m):
         if(not enb.has_key(y)):
             enb[y]=[]
         enb[y].append(j)
+    j+=1
 
-vst={}
-vst[0]=1
+#print "ed:",ed
+#print "enb:",enb
+#print "----------"
+#print
+
+uvst = {}
+for i in xrange(n-1):
+    uvst[i+1] = 1
+
 q = {}
-for i in xrange(n):
-    q[i] = 1
-el={}
 for i in enb[0]:
-    el.append(i)
-nm = 0
-dm = 0
-while(len(q)!=0):
-    mst = -float("inf")
-    nd = -1
-    for i in el.keys():
-        t = float(nm+(ed[i][3]))/(dm+(ed[i][4]))
-        if(t>mst):
-            if(not vst.has_key(ed[i][0])):
-                mst = t
-                nd = ed[i][0]
-                ce = i
-            if(not vst.has_key(ed[i][1])):
-                mst = t
-                nd = ed[i][1]
-                ce = i
-    vst[nd] = 1
-    nm+=(ed[ce][2])
-    dm+=(ed[ce][3])
+    q[i] = 1
 
-    for i in enb[nd]:
-        if(not el.has_key(i)):
-            el[i] = 1
+#print "uvst:",uvst
+#print "q:",q
+#print "enb[0]:",enb[0]
+#print "----------"
+#print
 
-    el.pop(i,None)
-    q.pop(nd,None)
+def fn(uvst,q,nm,dm):
+    if(len(uvst)==0):
+        #print "uvst:",uvst
+        #print "q:",q
+        #print "returning (nm,dm):", (nm,dm)
+        #print
+        return (nm,dm)
+    mnm = nm
+    mdm = dm
+    fr = -float("inf")
+    for i in q.keys():
+        tuvst = {}
+        for j in uvst.keys():
+            tuvst[j] = 1
+        tq = {}
+        for j in q.keys():
+            tq[j] = 1
+        if(uvst.has_key(ed[i][0])):
+            for j in enb[ed[i][0]]:
+                if (tuvst.has_key(ed[j][0])) or (tuvst.has_key(ed[j][1])):
+                    tq[j] = 1
+            tuvst.pop(ed[i][0],None)
+            tq.pop(i,None)
+            tnm, tdm = fn(tuvst, tq, nm+ed[i][2], dm+ed[i][3])
+            if(float(tnm)/(tdm)>fr):
+                mnm = tnm
+                mdm = tdm
+                fr = float(mnm)/mdm
 
+        elif(uvst.has_key(ed[i][1])):
+            for j in enb[ed[i][1]]:
+                if (tuvst.has_key(ed[j][0])) or (tuvst.has_key(ed[j][1])):
+                    tq[j] = 1
+            tuvst.pop(ed[i][1],None)
+            tq.pop(i,None)
+            tnm, tdm = fn(tuvst, tq, nm+ed[i][2], dm+ed[i][3])
+            if(float(tnm)/(tdm)>fr):
+                mnm = tnm
+                mdm = tdm
+                fr = float(mnm)/mdm
+    #print "uvst:",uvst
+    #print "q:", q
+    #print "nm,dm:",nm,dm
+    #print "mnm,mdm:",mnm,mdm
+    #print
+    return (mnm,mdm)
+
+nm,dm = fn(uvst,q,0,0)
 print str(nm)+"/"+str(dm)
