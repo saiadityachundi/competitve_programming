549cc23a4e6655394b4b425093c8d59836122f6e
Sai Aditya Chundi <saiaditya.chundi@gmail.com>
2017-04-17 13:13:20 +0530

    Revisiting the poles question from weekOfCode30. I have written the DP code in bottomUp fashion(iteration(which I still need to master)). Now, I have to optimise it further using ConvexHullTrick, which I learnt only recently.

--- hackerrank/week-of-code-30/poles/main.py
+++ hackerrank/week-of-code-30/poles/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2
 
-n,k = map(int, raw_input().split())
+n,kl = map(int, raw_input().split())
 al=[]
 wt=[]
 
@@ -23,26 +23,55 @@ while(i<n):
     mcs.append(mcs[i-1]+wt[i]*(al[i]-al[0]))
     i+=1
 
-def msc(i,j):
+def msc(i,j):               # j>i. move ind_j to ind_i
     return (mcs[j]-((al[i]-al[0])*(wsm[j]-wsm[i]))-mcs[i])
 
-pr=[]
-pr.append(0)
+#pr=[]
+#pr.append(0)
 
 mp={}
 
+#------------------------------------
+
+mc = {}
+mc[(n-1,1)] = 0
+
+
+i = n-2
+while(i>-1):
+    mc[(i,1)] = msc(i,n-1)
+    j = 2
+    while(j<=n-i):
+        mc[(i,j)] = float("inf")
+        k = i+1
+        while(k<=n-j+1):
+            mc[(i,j)] = min(mc[(i,j)],mc[(k,j-1)]+msc(i,k-1))
+            k+=1
+        j+=1
+    i-=1
+
+#------------------------------------
+
+    '''
 def mc(ind, k):
     if(mp.has_key((ind,k))):
         return mp[(ind,k)]
-    if(ind+k>n):
-        mp[(ind,k)]=(n,float("inf"))
+    if(ind+1<k):
+        mp[(ind,k)]=(ind,float("inf"))
         return mp[(ind,k)]
-    if(ind+k == 0):
+    if(ind+1 == k):
         mp[(ind,k)]=(ind,0)
         return mp[(ind,k)]
-    if(k==0):
-        mp[(ind,k)]=(n,0)
-        return mp[(ind,k)]
+    if(k==1):
+        if(ind>-1):
+            mp[(ind,k)]=(0,0)
+            return mp[(ind,k)]
+        else:
+            mp[(ind,k)]=(0,float("inf"))
+            return mp[(ind,k)]
+    '''
+
+    '''
     if(ind==n-1):
         if(k==1):
             mp[(ind,k)]=(n-1,0)
@@ -50,11 +79,26 @@ def mc(ind, k):
         else:
             mp[(ind,k)]=(n,float("inf"))
             return mp[(ind,k)]
-    if(ind==0):
-        x,y = mc(1,k-1)
-        mp[(ind,k)]=(0,y+msc(0,x-1))
-        return mp[(ind,k)]
+    '''
 
+    '''
+    if(ind==0):
+        if(k==1):
+            mp[(ind,k)]=(0,0)
+            return mp[(ind,k)]
+
+    ns1,m1 = mc(ind-1,k-1)
+    ns2,m2 = mc(ind-1,k)
+    m3 = m1+msc(ns1,ind-1)
+    m4 = m2+msc(ns2,ind)
+    if(m3<m4):
+        mp[(ind,k)]=(ind,m3)
+    else:
+        mp[(ind,k)]=(ns2,m2)
+    return mp[(ind,k)]
+    '''
+
+    '''
     prt=pr[0]
     pr[0]=ind
     ns1,m1=mc(ind+1,k-1)
@@ -69,10 +113,17 @@ def mc(ind, k):
     else:
         mp[(ind,k)]=(ns2,m2)
         return mp[(ind,k)]
+    '''
 
 '''
 for i in xrange(n):
     print "i,j:",i,i,msc(i,i)
 '''
-x,y = mc(0,k)
-print y
+
+#x,y = mc(n-1,k)
+#print y+msc(x,n-1)
+
+#for i in xrange(n):
+#    print msc(0,i)
+
+print mc[(0,kl)]
