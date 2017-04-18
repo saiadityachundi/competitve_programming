99b35019bd34d5c2361333962e1378b6297aa71d
Sai Aditya Chundi <saiaditya.chundi@gmail.com>
2017-04-18 15:19:28 +0530

    written the code using "CovexHullTrick".There seems to be a bug.

--- hackerrank/week-of-code-30/poles/main.py
+++ hackerrank/week-of-code-30/poles/main.py
@@ -26,17 +26,10 @@ while(i<n):
 def msc(i,j):               # j>i. move ind_j to ind_i
     return (mcs[j]-((al[i]-al[0])*(wsm[j]-wsm[i]))-mcs[i])
 
-#pr=[]
-#pr.append(0)
-
-mp={}
-
-#------------------------------------
-
 mc = {}
 mc[(n-1,1)] = 0
 
-
+'''
 i = n-2
 while(i>-1):
     mc[(i,1)] = msc(i,n-1)
@@ -45,85 +38,94 @@ while(i>-1):
         mc[(i,j)] = float("inf")
         k = i+1
         while(k<=n-j+1):
-            mc[(i,j)] = min(mc[(i,j)],mc[(k,j-1)]+msc(i,k-1))
+            ck = mc[(k,j-1)] + mcs[k-1] -mcs[i]
+            mk = wsm[i]-wsm[k-1]
+            x = (al[i]-al[0])
+            #mc[(i,j)] = min(mc[(i,j)],mc[(k,j-1)]+mcs[k-1]-(al[i]-al[0])*(wsm[k-1]-wsm[i])-mcs[i])
+            mc[(i,j)] = min(mc[(i,j)], mk*x+ck)
             k+=1
         j+=1
     i-=1
-
-#------------------------------------
-
-    '''
-def mc(ind, k):
-    if(mp.has_key((ind,k))):
-        return mp[(ind,k)]
-    if(ind+1<k):
-        mp[(ind,k)]=(ind,float("inf"))
-        return mp[(ind,k)]
-    if(ind+1 == k):
-        mp[(ind,k)]=(ind,0)
-        return mp[(ind,k)]
-    if(k==1):
-        if(ind>-1):
-            mp[(ind,k)]=(0,0)
-            return mp[(ind,k)]
-        else:
-            mp[(ind,k)]=(0,float("inf"))
-            return mp[(ind,k)]
-    '''
-
-    '''
-    if(ind==n-1):
-        if(k==1):
-            mp[(ind,k)]=(n-1,0)
-            return mp[(ind,k)]
-        else:
-            mp[(ind,k)]=(n,float("inf"))
-            return mp[(ind,k)]
-    '''
-
-    '''
-    if(ind==0):
-        if(k==1):
-            mp[(ind,k)]=(0,0)
-            return mp[(ind,k)]
-
-    ns1,m1 = mc(ind-1,k-1)
-    ns2,m2 = mc(ind-1,k)
-    m3 = m1+msc(ns1,ind-1)
-    m4 = m2+msc(ns2,ind)
-    if(m3<m4):
-        mp[(ind,k)]=(ind,m3)
-    else:
-        mp[(ind,k)]=(ns2,m2)
-    return mp[(ind,k)]
-    '''
-
-    '''
-    prt=pr[0]
-    pr[0]=ind
-    ns1,m1=mc(ind+1,k-1)
-    pr[0]=prt
-    ns2,m2=mc(ind+1,k)
-    m3=m1+msc(pr[0],ind-1)+msc(ind,ns1-1)
-    m4=m2+msc(pr[0],ns2-1)
-
-    if(m3<m4):
-        mp[(ind,k)]=(ind,m1+msc(ind,ns1-1))
-        return mp[(ind,k)]
-    else:
-        mp[(ind,k)]=(ns2,m2)
-        return mp[(ind,k)]
-    '''
-
-'''
-for i in xrange(n):
-    print "i,j:",i,i,msc(i,i)
 '''
 
-#x,y = mc(n-1,k)
-#print y+msc(x,n-1)
+def insxn(m1,c1,m2,c2):
+    return float(c2-c1)/(m1-m2)
 
-#for i in xrange(n):
-#    print msc(0,i)
+def addl((m,c),e):              # slopes come in decreasing order
+    if len(e) == 0:
+        e.append((m,c,-float("inf"),float("inf")))
+    elif len(e) == 1:
+        m1,c1,x1,y1 = e[0]
+        if(m1==m):
+            if(c>c1):
+                e[0] = (m,c,-float("inf"),float("inf"))
+        else:
+            x = insxn(m,c,me1,c1)
+            e[0] = (m1,c1,-float("inf"),x)
+            e.append(m,c,x,float("inf"))
+    else:
+        m1,c1,x1,y1 = e[-1]
+        if(m1==m):
+            if(c>c1):
+                e.pop(-1)
+                addl((m,c),e)
+        else:
+            x = insxn(m,c,m1,c1)
+            if(x<=x1):
+                e.pop(-1)
+                addl((m,c),e)
+            else:
+                e[-1] = (m1,c1,x1,x)
+                e.append(m,c,x,float("inf"))
+
+def hval(x,e,st,en):
+    if(en==st+1):
+        if(x<=e[st][3]):
+            return e[st][0]*x+e[st][1]
+        else:
+            return e[en][0]*x+e[en][1]
+    md = (st+en)/2
+    if e[md][2]<=x and x<=e[md][3]:
+        return e[md][0]*x+e[md][1]
+    elif(x<e[md][2]):
+        return hval(x,e,st,md)
+    elif(x>e[md][3]):
+        return hval(x,e,md,en)
+
+j = 1
+i = n-1
+while(i>-1):
+    mc[(i,j)] = msc(i,n-1)
+    print "mc[("+str(i)+","+str(j)+")]: "+str(mc[(i,j)])
+    i-=1
+
+j = 2
+while(j<=kl):
+    i = n-j
+    e = []
+    while(i>=0):
+        k = i+1
+        mk = wsm[i]-wsm[k-1]                            # slopes come in descending order
+        ck = mc[(k,j-1)] + mcs[k-1] - mcs[i]
+        x = (al[i]-al[0])
+        addl((mk,ck),e)
+
+        mc[(i,j)] = hval(x,e,0,len(e)-1)
+        print "mc[("+str(i)+","+str(j)+")]: "+str(mc[(i,j)])
+
+        # ---------------------------------------------------------------------------------------- #
+        #mc[(i,j)] = float("inf")
+        #k = i+1
+        #while(k<=n-j+1):
+        #    ck = mc[(k,j-1)] + mcs[k-1] -mcs[i]
+        #    mk = wsm[i]-wsm[k-1]
+        #    x = (al[i]-al[0])
+        #    #mc[(i,j)] = min(mc[(i,j)],mc[(k,j-1)]+mcs[k-1]-(al[i]-al[0])*(wsm[k-1]-wsm[i])-mcs[i])
+        #    mc[(i,j)] = min(mc[(i,j)], mk*x+ck)
+        #    k+=1
+        # ---------------------------------------------------------------------------------------- #
+
+        i-=1
+    j+=1
 
 print mc[(0,kl)]
