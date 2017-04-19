ac2b2f1df0fc7acb6567f74ad4629f03fe7e29ec
Sai Aditya Chundi <saiaditya.chundi@gmail.com>
2017-04-19 14:24:37 +0530

    reduced the memory used by the program, as mentioned earlier. All cases now run without giving any runtime errors. But the later cases timeout. I am now going to add the convex hull part of the code, and test

--- hackerrank/week-of-code-30/poles/main.cpp
+++ hackerrank/week-of-code-30/poles/main.cpp
@@ -7,7 +7,15 @@
 using namespace std;
 typedef long long ll;
 typedef long double ld;
+//struct line_s{
+//    ll m,c;
+//    ld xl,xr;
+//    inline ll get(ll x){
+//        return m*x+c;
+//    }
+//};
 ll N,K;
+//deque<ll> lines;
 
 //inline ll msc(ll *mcs,ll i,ll j){
 //    return (mcs[j]-mcs[i]-((al[i]-al[0])*(wsm[j]-wsm[i])));
@@ -16,7 +24,7 @@ ll N,K;
 int main(){
     cin >> N >> K;
     ll al[N],wt[N],wsm[N],mcs[N];;
-    ll mc[N][K+1];
+    ll mc[N][2];//ll mc[N][K+1];
     ll i,j,k;
     for(i=0;i<N;i++){
         cin >> al[i] >> wt[i];
@@ -34,32 +42,37 @@ int main(){
 
     ll ck,mk,x;
     
-    j = 1;
+    j = 0;
     i = N-1;
     while(i>-1){
         mc[i][j] = /*msc(mcs,i,N-1);*/(mcs[N-1]-mcs[i]-((al[i]-al[0])*(wsm[N-1]-wsm[i])));
         i--;
     }
 
+    for(ll p=0;p<N;p++)
+        mc[p][1]=mc[p][0];
+
     j = 2;
     while(j<=K){
        i = N-j;
        while(i>=0){
-           mc[i][j] = LLONG_MAX;
+           mc[i][1] = LLONG_MAX;
            k = i+1;
            while(k<=N-j+1){
-               ck = mc[k][j-1]+mcs[k-1]-mcs[i]+(al[i]-al[0])*wsm[i];
+               ck = mc[k][0]+mcs[k-1]-mcs[i]+(al[i]-al[0])*wsm[i];
                mk = wsm[k-1];
                x = -(al[i]-al[0]);
-               mc[i][j] = min(mc[i][j],mk*x+ck);
+               mc[i][1] = min(mc[i][1],mk*x+ck);
                k+=1;
            }
            i-=1;
        }
+       for(ll p=0;p<N;p++)
+           mc[p][0]=mc[p][1];
        j+=1;
     }
 
-    cout << mc[0][K];
+    cout << mc[0][1];
 
     return 0;
 }
